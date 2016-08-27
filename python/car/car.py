06b9760abe6e6bbceaf1f5eb847def95affbfe9f
willywsm1013 <willywsm1013@gmail.com>
2016-08-27 15:59:23 +0800

    use constant number instead of real number to represent speed

--- python/car/car.py
+++ python/car/car.py
@@ -1,10 +1,13 @@
 class Car:
     def __init__(self, arduino):
-        self.arduino = arduino
+        self.ROTATE_SPEED = 10
+		self.BASE_SPEED = 20
+		self.MAX_SPEED = 90
+		self.arduino = arduino
         self.lastAngle = 0
         self.p = 0.5
         self.i = 0
-        self.d = 1
+        self.d = 0
         self.accum = 0
         self.rSpeed = 0
         self.lSpeed = 0
@@ -12,8 +15,8 @@ class Car:
 
     def forward(self, angle):
         self.accum += angle
-        self.rSpeed = 20 - self.p * angle - self.i * self.accum - self.d * (angle-self.lastAngle)
-        self.lSpeed = 20 + self.p * angle + self.i * self.accum + self.d * (angle-self.lastAngle)
+        self.rSpeed = self.BASE_SPEED - self.p * angle - self.i * self.accum - self.d * (angle-self.lastAngle)
+        self.lSpeed = self.BASE_SPEED + self.p * angle + self.i * self.accum + self.d * (angle-self.lastAngle)
         self.toArduino()
 
     def setSpeed(self, right, left):
@@ -26,14 +29,14 @@ class Car:
         self.toArduino()
 
     def rotate(self, angle):
-        self.rSpeed = 10
-        self.lSpeed = 10
+        self.rSpeed = self.ROTATE_SPEED
+        self.lSpeed = self.ROTATE_SPEED
         if angle > 0:
             self.rSpeed *= -1
         elif angle < 0:
-            self.lSPeed += -1
+            self.lSPeed *= -1
         else:
-            self.rSpeed, self.lSpeed = 20, 20
+            self.rSpeed, self.lSpeed = self.BASE_SPEED, self.BASE_SPEED
         self.toArduino()
 
     def stop(self):
@@ -42,14 +45,14 @@ class Car:
         self.toArduino()
 
     def toArduino(self):
-        if self.rSpeed < -90:
-            self.rSpeed = -90
-        elif self.rSpeed > 90:
-            self.rSpeed = 90
-        if self.lSpeed < -90:
-            self.lSpeed = -90
-        elif self.lSpeed > 90:
-            self.lSpeed = 90
+        if self.rSpeed < -MAX_SPEED:
+            self.rSpeed = -MAX_SPEED
+        elif self.rSpeed > MAX_SPEED:
+            self.rSpeed = MAX_SPEED
+        if self.lSpeed < -MAX_SPEED:
+            self.lSpeed = -MAX_SPEED
+        elif self.lSpeed > MAX_SPEED:
+            self.lSpeed = MAX_SPEED
         command = 's ' + str(int(self.rSpeed)) + ',' + str(int(self.lSpeed)) + '\n'
         if self.arduino.available() == True:
             self.arduino.push(command)
